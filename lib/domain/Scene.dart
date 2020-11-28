ebfbbf848f22231635d3b2492c2685c62d2d419c
Aromal <aromalsasidharan@gmail.com>
2020-11-28 19:42:27 +0800

    add profile design clipper and make modular

---
+++ lib/domain/Scene.dart
@@ -0,0 +1,8 @@
+
+part of 'Domain.dart';
+
+
+enum Scene {
+  profile,
+  contact
+}
---
+++ modules/user_profile/lib/src/profile_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+
+class ProfilePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ProfilePageState();
+  }
+
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return SizedBox(width: double.infinity,
+      height: double.infinity,
+      child: Container(
+       color: Colors.amberAccent,
+      ));
+  }
+
+}
